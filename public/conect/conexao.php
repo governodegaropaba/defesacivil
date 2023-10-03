9d4b509ee0d7cb8bd66f4303fd661fc8271ef920
Luis Armando Paglioza <34868901+paglioza@users.noreply.github.com>
2023-10-03 14:34:32 -0300

    Delete public/conect directory

--- public/conect/conexao.php
+++
@@ -1,9 +0,0 @@
-
- <?php
-$link = pg_connect("host=dbserver.prefa.br port=5432 dbname=defesacivil user=informatica password=gsul@10");
-if (!$link) {
-	die('Erro ao conectar com o banco PGSQL do iEducar!');
-}//else{
-//	echo "Conectado com sucesso";
-//}
-?> 
